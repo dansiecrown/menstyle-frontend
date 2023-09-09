52126d7f6aac8c7fde047c4ea81b33953edb0feb
dansiecrown <dansiecrown@gmail.com>
2023-09-09 13:49:00 +0100

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import Button from 'react-bootstrap/Button';
 import { getError } from './utils';
 import axios from 'axios';
 import SearchBox from './components/SearchBox';
-import SearchScreen from './screens/SearchScreen';
+// import SearchScreen from './screens/SearchScreen';
 import ProtectedRoute from './components/ProtectedRoute';
 import DashboardScreen from './screens/DashboardScreen';
 import AdminRoute from './components/AdminRoute';
@@ -63,252 +63,252 @@ function App() {
     fetchCategories();
   }, []);
   return (
-    
+
+    <div
+      className={
+        sidebarIsOpen
+          ? fullBox
+            ? "site-container active-cont d-flex flex-column full-box w-100vw"
+            : "site-container active-cont d-flex flex-column"
+          : fullBox
+            ? "site-container d-flex flex-column full-box"
+            : "site-container d-flex flex-column w-100vw"
+      }
+    >
+      <ToastContainer position="bottom-center" limit={1} />
+
+      <header
+        style={{ width: "100vw", position: "sticky", top: 0, zIndex: "1" }}
+      >
+        <Navbar bg="primary" variant="light" expand="lg">
+          <Container>
+            <Button
+              variant=""
+              onClick={() => setSidebarIsOpen(!sidebarIsOpen)}
+            >
+              <i className="fas fa-bars"></i>
+            </Button>
+            <LinkContainer to="/">
+              <Navbar.Brand>
+                {" "}
+                <h1 className="heading">ABS Style Spot</h1>
+              </Navbar.Brand>
+            </LinkContainer>
+
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto  w-100  justify-content-end">
+                <Link to="/cart" className="nav-link">
+                  <span>
+                    <i
+                      className="bi bi-cart4"
+                      style={{ color: "#fff", fontSize: "3rem" }}
+                    ></i>
+                  </span>
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </Badge>
+                  )}
+                </Link>
+
+                <nav className='info'>
+                  {" "}
+                  {userInfo ? (
+                    <NavDropdown
+                      title={userInfo.name}
+                      id="basic-nav-dropdown"
+                    >
+                      <LinkContainer to="/orderhistory" style={{ marginLeft: '25px' }}>
+                        <NavDropdown.Item>Order History</NavDropdown.Item>
+                      </LinkContainer>
+                      <NavDropdown.Divider />
+                      <Link
+                        className="dropdown-item"
+                        to="#signout"
+                        onClick={signoutHandler}
+                      >
+                        <p>Sign Out</p>
+                      </Link>
+                    </NavDropdown>
+                  ) : (
+                    <Link className="nav-link" to="/signin">
+                      <nav className='signin'>Sign In</nav>
+                    </Link>
+                  )}{" "}
+                </nav>
+                <nav className='info'>
+                  {" "}
+                  {userInfo && userInfo.isAdmin && (
+                    <NavDropdown title="Admin" id="admin-nav-dropdown">
+                      <LinkContainer to="/admin/dashboard">
+                        <NavDropdown.Item>Dashboard</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/admin/products">
+                        <NavDropdown.Item>Products</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/profile">
+                        <NavDropdown.Item>User Profile</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/admin/orders">
+                        <NavDropdown.Item>Orders</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/admin/users">
+                        <NavDropdown.Item>Users</NavDropdown.Item>
+                      </LinkContainer>
+                    </NavDropdown>
+                  )}
+                </nav>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <nav className="sub-header">
+          <SearchBox /></nav>
+      </header>
       <div
         className={
           sidebarIsOpen
-            ? fullBox
-              ? "site-container active-cont d-flex flex-column full-box w-100vw"
-              : "site-container active-cont d-flex flex-column"
-            : fullBox
-            ? "site-container d-flex flex-column full-box"
-            : "site-container d-flex flex-column w-100vw"
+            ? "active-nav side-navbar d-flex justify-content-between flex-wrap flex-column w-100vh"
+            : "side-navbar d-flex justify-content- between flex-wrap flex-column w-100vh "
         }
       >
-        <ToastContainer position="bottom-center" limit={1} />
-
-        <header
-          style={{ width: "100vw", position: "sticky", top: 0, zIndex: "1" }}
-        >
-          <Navbar bg="primary" variant="light" expand="lg">
-            <Container>
-              <Button
-                variant=""
-                onClick={() => setSidebarIsOpen(!sidebarIsOpen)}
-              >
-                <i className="fas fa-bars"></i>
-              </Button>
-              <LinkContainer to="/">
-                <Navbar.Brand>
-                  {" "}
-                  <h1 className="heading">ABS Style Spot</h1>
-                </Navbar.Brand>
-              </LinkContainer>
-
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto  w-100  justify-content-end">
-                  <Link to="/cart" className="nav-link">
-                    <span>
-                      <i
-                        className="bi bi-cart4"
-                        style={{ color: "#fff", fontSize: "3rem" }}
-                      ></i>
-                    </span>
-                    {cart.cartItems.length > 0 && (
-                      <Badge pill bg="danger">
-                        {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
-                      </Badge>
-                    )}
-                  </Link>
-
-                  <nav className='info'>
-                    {" "}
-                    {userInfo ? (
-                      <NavDropdown
-                        title={userInfo.name}
-                        id="basic-nav-dropdown"
-                      >
-                        <LinkContainer to="/orderhistory" style={{marginLeft:'25px'}}>
-                          <NavDropdown.Item>Order History</NavDropdown.Item>
-                        </LinkContainer>
-                        <NavDropdown.Divider />
-                        <Link
-                          className="dropdown-item"
-                          to="#signout"
-                          onClick={signoutHandler}
-                        >
-                          <p>Sign Out</p>
-                        </Link>
-                      </NavDropdown>
-                    ) : (
-                      <Link className="nav-link" to="/signin">
-                        <nav className='signin'>Sign In</nav>
-                      </Link>
-                    )}{" "}
-                  </nav>
-                  <nav className='info'>
-                    {" "}
-                    {userInfo && userInfo.isAdmin && (
-                      <NavDropdown title="Admin" id="admin-nav-dropdown">
-                        <LinkContainer to="/admin/dashboard">
-                          <NavDropdown.Item>Dashboard</NavDropdown.Item>
-                        </LinkContainer>
-                        <LinkContainer to="/admin/products">
-                          <NavDropdown.Item>Products</NavDropdown.Item>
-                        </LinkContainer>
-                        <LinkContainer to="/profile">
-                          <NavDropdown.Item>User Profile</NavDropdown.Item>
-                        </LinkContainer>
-                        <LinkContainer to="/admin/orders">
-                          <NavDropdown.Item>Orders</NavDropdown.Item>
-                        </LinkContainer>
-                        <LinkContainer to="/admin/users">
-                          <NavDropdown.Item>Users</NavDropdown.Item>
-                        </LinkContainer>
-                      </NavDropdown>
-                    )}
-                  </nav>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-          <nav className="sub-header">
-          <SearchBox /></nav>
-        </header>
-         <div
-          className={
-            sidebarIsOpen
-              ? "active-nav side-navbar d-flex justify-content-between flex-wrap flex-column w-100vh"
-              : "side-navbar d-flex justify-content- between flex-wrap flex-column w-100vh "
-          }
-        >
-          <div className="sticky">
-            <Nav className="flex-column text-white w-100 p-5">
-              <Nav.Item>
-                <strong>Categories</strong>
+        <div className="sticky">
+          <Nav className="flex-column text-white w-100 p-5">
+            <Nav.Item>
+              <strong>Categories</strong>
+            </Nav.Item>
+            {categories && categories.map((category) => (
+              <Nav.Item key={category.key}>
+                <LinkContainer
+                  to={{ pathname: "/search", search: `category=${category}` }}
+                  onClick={() => setSidebarIsOpen(false)}
+                >
+                  <Nav.Link>{category}</Nav.Link>
+                </LinkContainer>
               </Nav.Item>
-              {categories && categories.map((category) => (
-                <Nav.Item key={category.key}>
-                  <LinkContainer
-                    to={{ pathname: "/search", search: `category=${category}` }}
-                    onClick={() => setSidebarIsOpen(false)}
-                  >
-                    <Nav.Link>{category}</Nav.Link>
-                  </LinkContainer>
-                </Nav.Item>
-              ))}
-            </Nav>
-          </div>
+            ))}
+          </Nav>
         </div>
-        <main>
-          <Container className="mt-3">
-            <Routes>
-              <Route path="/product/:slug" element={<ProductScreen />} />
-              <Route path="/cart" element={<CartScreen />} />
-
-              <Route path="/search" element={<SearchScreen />} />
-              <Route path="/signin" element={<SigninScreen />} />
-              <Route path="/signup" element={<SignupScreen />} />
-              {/* <Route path="/display" element={<DisplayScreen />} /> */}
-              <Route
-                path="/forget-password"
-                element={<ForgetPasswordScreen />}
-              />
-              <Route
-                path="/reset-password/:token"
-                element={<ResetPasswordScreen />}
-              />
-
-              <Route
-                path="/profile"
-                element={
-                  <ProtectedRoute>
-                    <ProfileScreen />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/map"
-                element={
-                  <ProtectedRoute>
-                    <MapScreen />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/placeorder" element={<PlaceOrderScreen />} />
-              <Route
-                path="/order/:id"
-                element={
-                  <ProtectedRoute>
-                    <OrderScreen />{" "}
-                  </ProtectedRoute>
-                }
-              ></Route>
-              <Route
-                path="/orderhistory"
-                element={
-                  <ProtectedRoute>
-                    {" "}
-                    <OrderHistoryScreen />
-                  </ProtectedRoute>
-                }
-              ></Route>
-              <Route
-                path="/shipping"
-                element={<ShippingAddressScreen />}
-              ></Route>
-              <Route path="/payment" element={<PaymentMethodScreen />}></Route>
-
-              {/* Admin Routes */}
-              <Route
-                path="/admin/dashboard"
-                element={
-                  <AdminRoute>
-                    <DashboardScreen />
-                  </AdminRoute>
-                }
-              ></Route>
-              <Route
-                path="/admin/orders"
-                element={
-                  <AdminRoute>
-                    <OrderListScreen />
-                  </AdminRoute>
-                }
-              ></Route>
-              <Route
-                path="/admin/users"
-                element={
-                  <AdminRoute>
-                    <UserListScreen />
-                  </AdminRoute>
-                }
-              ></Route>
-              <Route
-                path="/admin/products"
-                element={
-                  <AdminRoute>
-                    <ProductListScreen />
-                  </AdminRoute>
-                }
-              ></Route>
-              <Route
-                path="/admin/product/:id"
-                element={
-                  <AdminRoute>
-                    {" "}
-                    <ProductEditScreen />
-                  </AdminRoute>
-                }
-              ></Route>
-              <Route
-                path="/admin/user/:id"
-                element={
-                  <AdminRoute>
-                    <UserEditScreen />
-                  </AdminRoute>
-                }
-              ></Route>
-
-              <Route path="/" element={<HomeScreen />} />
-            </Routes>
-          </Container>
-        </main>
-        <footer>
-          <div className="text-center footer">All rights reserved</div>
-        </footer>
       </div>
+      <main>
+        <Container className="mt-3">
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/cart" element={<CartScreen />} />
+
+            {/* <Route path="/search" element={<SearchScreen />} /> */}
+            <Route path="/signin" element={<SigninScreen />} />
+            <Route path="/signup" element={<SignupScreen />} />
+            {/* <Route path="/display" element={<DisplayScreen />} /> */}
+            <Route
+              path="/forget-password"
+              element={<ForgetPasswordScreen />}
+            />
+            <Route
+              path="/reset-password/:token"
+              element={<ResetPasswordScreen />}
+            />
+
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <ProfileScreen />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/map"
+              element={
+                <ProtectedRoute>
+                  <MapScreen />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/placeorder" element={<PlaceOrderScreen />} />
+            <Route
+              path="/order/:id"
+              element={
+                <ProtectedRoute>
+                  <OrderScreen />{" "}
+                </ProtectedRoute>
+              }
+            ></Route>
+            <Route
+              path="/orderhistory"
+              element={
+                <ProtectedRoute>
+                  {" "}
+                  <OrderHistoryScreen />
+                </ProtectedRoute>
+              }
+            ></Route>
+            <Route
+              path="/shipping"
+              element={<ShippingAddressScreen />}
+            ></Route>
+            <Route path="/payment" element={<PaymentMethodScreen />}></Route>
+
+            {/* Admin Routes */}
+            <Route
+              path="/admin/dashboard"
+              element={
+                <AdminRoute>
+                  <DashboardScreen />
+                </AdminRoute>
+              }
+            ></Route>
+            <Route
+              path="/admin/orders"
+              element={
+                <AdminRoute>
+                  <OrderListScreen />
+                </AdminRoute>
+              }
+            ></Route>
+            <Route
+              path="/admin/users"
+              element={
+                <AdminRoute>
+                  <UserListScreen />
+                </AdminRoute>
+              }
+            ></Route>
+            <Route
+              path="/admin/products"
+              element={
+                <AdminRoute>
+                  <ProductListScreen />
+                </AdminRoute>
+              }
+            ></Route>
+            <Route
+              path="/admin/product/:id"
+              element={
+                <AdminRoute>
+                  {" "}
+                  <ProductEditScreen />
+                </AdminRoute>
+              }
+            ></Route>
+            <Route
+              path="/admin/user/:id"
+              element={
+                <AdminRoute>
+                  <UserEditScreen />
+                </AdminRoute>
+              }
+            ></Route>
+
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <footer>
+        <div className="text-center footer">All rights reserved</div>
+      </footer>
+    </div>
   );
 }
 
